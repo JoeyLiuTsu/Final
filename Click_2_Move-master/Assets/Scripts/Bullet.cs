3efe2c94d04034f7b1a5b1e7931bbe0775859e5f
Joey Liu <46060748+JoeyLiuTsu@users.noreply.github.com>
2019-12-20 23:53:10 -0800

    I tried but no time

---
+++ Click_2_Move-master/Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float force = 15f;
+    private Enemy host;
+    private void Start()
+    {
+        host = GetComponent<Enemy>();
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Enemy.toTarget* force, ForceMode.Impulse);
+    }
+
+
+    
+       
+        
+        
+        
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
